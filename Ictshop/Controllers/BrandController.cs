739a93e702bc9ef069c056be578a5c3d3dac6f6c
Newt1712 <ankvhe150757@fpt.edu.vn>
2022-03-24 14:56:24 +0700

    Add search feature for Brands

--- Ictshop/Controllers/BrandController.cs
+++ Ictshop/Controllers/BrandController.cs
@@ -4,16 +4,28 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Ictshop.Models;
+using Newtonsoft.Json;
 
 namespace Ictshop.Controllers
 {
     public class BrandController : Controller
     {
         ShopManagement db = new ShopManagement();
-        public ActionResult DanhmucPartial()
+        public ActionResult BrandPartial()
         {
-            var danhmuc = db.Brands.ToList();
-            return PartialView(danhmuc);
+            var brands = db.Brands.ToList();
+            return PartialView(brands);
         }
+
+        /*public JsonResult GetByName(String name)
+        {
+            List<Brand> brands = db.Brands.ToList();
+            if (!String.IsNullOrEmpty(name))
+            {
+                brands = brands.FindAll(b => b.BrandName.StartsWith(name.Trim(), StringComparison.OrdinalIgnoreCase));
+            }
+            var json = JsonConvert.SerializeObject(brands);
+            return Json(brands, JsonRequestBehavior.AllowGet);
+        }*/
     }
 }
