10177000315267eb5e6131a9c22445a6f5285966
Newt1712 <ankvhe150757@fpt.edu.vn>
2022-03-25 19:54:10 +0700

    Upload photo in Brand

--- Ictshop/Areas/Admin/Controllers/BrandController.cs
+++ Ictshop/Areas/Admin/Controllers/BrandController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -59,14 +60,13 @@ namespace Ictshop.Areas.Admin.Controllers
         }
 
         // POST: Admin/Brands/Create
-        // To protect from overposting attacks, please enable the specific properties you want TotalCost bind TotalCost, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "BrandID,BrandName")] Brand Brand)
+        public ActionResult Create([Bind(Include = "BrandID,BrandName")] Brand Brand, string img)
         {
             if (ModelState.IsValid)
             {
+                Brand.BrandImg = img;
                 db.Brands.Add(Brand);
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -91,11 +91,9 @@ namespace Ictshop.Areas.Admin.Controllers
         }
 
         // POST: Admin/Brands/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want TotalCost bind TotalCost, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "BrandID,BrandName")] Brand Brand)
+        public ActionResult Edit([Bind(Include = "BrandID,BrandName")] Brand Brand, string img)
         {
             if (ModelState.IsValid)
             {
